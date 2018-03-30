a85095f784a6dd4cdd4d56bd9d06fcd0919285ed
David Scherer-ODell <dscherer21@gmail.com>
2018-03-30 15:08:21 -0500

    added notes

---
+++ es6andreactnotes.js
@@ -0,0 +1,20 @@
+//if you dont assign "new" to this, it will put into global variables.
+var dog = new DigitalPal(true, false, false);
+
+// .map function creates a new array using data from the old array altered by the callback function taht you give it.
+var a = [1, 2, 3];
+
+
+//A stateful app/component is a program that saves client data from the activities of one session for use in the next session. ... The operating systems that applications run on are also typically stateful. As HTTP is inherently stateless, if state is needed for web apps, it must be built into them.
+
+//presentational/dumb components: Are concerned with how things look. May contain both presentational and container components** inside, and usually have some DOM markup and styles of their own. Often allow containment via this.props.children. Have no dependencies on the rest of the app, such as Flux actions or stores.
+
+//'props' is short for properties
+
+//be sure to assign a key property when you are assigning props.
+
+// Flux: A Pattern for Unidirectional Data Flow. As a complement to React, Facebook has proposed Flux as a starting point, though they describe it more as a pattern than a library, and at the recent ReactConf they already hinted at its impending deprecation.
+
+//TypeScript syntax is a superset of JavaScript(Ecmascript). ... The biggest distinctive feature is that TypeScript supports optional types. But ES6 or ES2015 is a subset of TypeScript - anything you write in ES2015 is valid TS. So main differences are type annotations, interface support and some much more robust tooling.
+
+// when importing  a component make sure to capitalize the first letter of the name of the component.
--- es6notes.js
+++
@@ -1,7 +0,0 @@
-//if you dont assign "new" to this, it will put into global variables.
-var dog = new DigitalPal(true, false, false);
-
-// .map function creates a new array using data from the old array altered by the callback function taht you give it.
-var a = [1, 2, 3];
-
-
